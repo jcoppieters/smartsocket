2872ac0af4a3857c0a49f0ea19568ebad2dbdcb9
Johan Coppieters <johan577@mac.com>
2019-12-30 17:29:34 +0100

    Inital commit

---
+++ README.md
@@ -0,0 +1,75 @@
+SmartSocket Server v1.0
+
+v1, dec 2019, Johan Coppieters
+
+Use:
+- start the server on a specific port (and IP address)
+- connect from a web app with a WebSocket
+- add to the ws url /[url]:[port] of the target TCP Socket
+- send/receive from the websocket as if it were a raw socket
+
+Client Example:
+
+  private socket: WebSocket;
+
+  async open() {
+    return new Promise((resolve, reject) => {
+      try {
+        ////////////////////////////////
+        // try to open the connection //
+        this.log("opening connection to the SmartSocket Server");
+        const wsserver = this.system.config.socketserver + ":" + this.system.config.socketport;
+        const tcpserver = this.config.address + ":" + this.config.port;
+        this.socket = new WebSocket("ws://" + wsserver + "/" + tcpserver);
+        if (!this.socket) throw(new Error("could create new socket"));
+
+        ///////////////////////
+        // set data listener //
+        this.socket.onmessage = (message) => {
+          // messages need to be buffered until "]" is received
+          this.handleData(message.data);
+        };
+
+        ///////////////////////////
+        // set an error listener //
+        this.socket.onerror = (err) => {
+          this.err("Socket: " + err);
+        };
+
+        ///////////////////////////////////////////
+        // set end: the server closed the socket //
+        this.socket.onclose = () => {
+          this.isOpen = false;
+          this.isLoggedIn = false;
+          this.log("end -> socket got disconnected");
+
+          if (!this.closeRequested) {
+            // unexpected close
+            this.err("Socket: closed unexpectedly");
+          }
+        };
+
+        this.socket.onopen = () => {
+          this.isOpen = true;
+
+          // request a connection to the real socket
+          this.log("connection open on " + this.config.address + " on port " + this.config.port);
+          
+          // resolve our promise with the opened socket
+          resolve(this.socket);
+        };
+
+
+      } catch(e) {
+        this.err("Failed to open a connection on port " + this.getPort());
+        reject(e);
+      }
+    });
+  }
+
+
+  try {
+    socket.send(data);
+  } catch(err) {
+    console.log("error sending through socket " + err.message);
+  }
---
+++ index.ts
@@ -0,0 +1,109 @@
+#!/usr/bin/env node
+
+import * as net from 'net';
+import * as http from 'http';
+import * as https from 'https';
+import * as fs from 'fs';
+import { Server as WSServer } from 'ws';
+
+const kPort = 9999;
+
+// Handle new WebSocket client
+function handleClient(client, req) {
+  let connected = false;
+  let target: net.Socket;
+  const clientAddr = client._socket.remoteAddress;
+  const log = (msg) => console.log(' ' + clientAddr + ': '+ msg);
+
+  /////////////////////////////////////
+  // url parsing to find ip and port
+  const url = (req ? req.url : client.upgradeReq.url).substr(1);
+  const parts = url.split(':');
+  if (parts.length != 2) {
+    log("invalid target address: " + url);
+    return;
+  }
+
+  log("websocket proxy to ip: " + parts[0] + ", port: " + parts[1]);
+
+  ////////////////////////////////
+  // Target connection handling 
+  target = net.createConnection(parts[1], parts[0], function() {
+    connected = true;
+    target.setNoDelay(); // disable Nagle
+    log("connected to target -> " + url);
+  });
+  target.on('data', function(data) {
+    const msg = data.toString();
+    log("received from target: " + msg.substr(0, msg.length-1));
+    try {
+      client.send(msg);
+    } catch(e) {
+      log("client sending error: " + e.message + ", cleaning up target");
+      target.end();
+    }
+  });
+  target.on('end', function() {
+      log('target disconnected');
+      client.close();
+      connected = false;
+  });
+  target.on('error', function(err) {
+      log('target error' + err);
+      target.end();
+      client.close();
+      connected = false;
+  });
+
+  ///////////////////////////////////
+  // websocket connection handling
+  client.on('message', function(msg) {
+    log('got message from websocket: ' + msg.substr(0, msg.length-1));
+    // Let's hope the socket is buffering even before the connection is fully open
+    // if (! connected) log('Not yet connected!!') else
+    target.write(msg);
+  });
+  client.on('close', function(code, reason) {
+    log('websocket disconnected: ' + code + ' [' + reason + ']');
+    target.end();
+  });
+  client.on('error', function(err) {
+    log('websocket error: ' + err);
+    target.end();
+  });
+}
+
+// Process an HTTP static file request
+function http_request(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.end('<html><h1>The Great SmartSocket</h1><p>Johan Coppieters, 2020</p></html>');
+}
+
+console.log("SmartSocket starting up.");
+
+let webServer: http.Server | https.Server;
+try {
+  // try https
+  const cert = fs.readFileSync('ssh.cert');
+  const key = fs.readFileSync('ssh.key');
+  webServer = https.createServer({cert, key}, http_request);
+  console.log(" - Running in encrypted HTTPS (wss://) mode using: ssh.cert, ssh.key");
+
+} catch(e) {
+  try {
+    // fallback to http
+    webServer = http.createServer(http_request);
+    console.log(" - Running in unencrypted HTTP (ws://) mode");
+
+  } catch(e) {
+    // give up
+    console.log(" - Can't start up: " + e.message);
+  }
+}
+
+if (webServer) {
+  webServer.listen(kPort, () => {
+    const wsServer = new WSServer({server: webServer});
+    wsServer.on('connection', handleClient);
+  });
+}
