23a94ab0deee56344b964abac454612201915b71
Johan Coppieters <johan577@mac.com>
2020-11-10 13:10:55 +0100

    typo in run9998

--- run9998.js
+++ run9998.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const socapp_1 = require("./server/socapp");
 console.log("run.ts - running in directory: " + process.cwd());
-const socServer = new socapp_1.SocApp(null, "server", console.log, 9999);
+const socServer = new socapp_1.SocApp(null, "server", console.log, 9998);
 socServer.serve();
 //# sourceMappingURL=run9998.js.map
--- run9998.ts
+++ run9998.ts
@@ -2,5 +2,5 @@ import { SocApp } from "./server/socapp";
 
 console.log("run.ts - running in directory: " + process.cwd());
 
-const socServer = new SocApp(null, "server", console.log, 9999);
+const socServer = new SocApp(null, "server", console.log, 9998);
 socServer.serve();
